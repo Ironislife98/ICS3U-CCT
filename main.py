00a5b19a51aa9ec63f77f9d9f599e5a22c0a4944
EmilioMendoza <Ironislife39@gmail.com>
2023-01-09 22:48:10 -0500

    Added inefficient method of drawing board

--- main.py
+++ main.py
@@ -6,20 +6,42 @@ pygame.init()
 
 WIDTH, HEIGHT = 900, 900
 win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Checkers")
 
 
 class Board:
     def __init__(self):
         self.vector = pygame.math.Vector2(0, 0)
         self.width = 15     # Width is number of squares
-        self.boxWidth = 10     # Box width is how wide the squares are
+        self.boxWidth = 45    # Box width is how wide the squares are
+        self.xoffset, self.yoffset = 110, 100
+        self.ranCalc = False
+        self.squares = {}
 
     def draw(self):
-        column = 0
-        row = 0
-        for square in range(self.width):
-            drawPos = pygame.math.Vector2(square * self.width)
+        if not self.ranCalc:
+            row, column = 0, 0
+            colors = [(255, 0, 0), (0, 255, 0)]
+            colorselection = True
+            for i in range(self.width * self.width):
+                pygame.draw.rect(win, colors[int(colorselection)],
+                                 pygame.Rect(column * self.boxWidth + self.xoffset, row * self.boxWidth + self.yoffset, self.boxWidth, self.boxWidth))
+                colorselection = not colorselection
+                if column == self.width - 1:
+                    column = 0
+                    row += 1
+                else:
+                    column += 1
 
+                if row == self.width:
+                    return
+                self.squares[str(i)] = colors[int(colorselection)], pygame.Rect(column * self.boxWidth + self.xoffset, row * self.boxWidth + self.yoffset, self.boxWidth, self.boxWidth)
+            print("finished")
+            self.ranCalc = True
+
+        else:
+            for key in self.squares:
+                pygame.draw.rect(win, self.squares[key][0], self.squares[key][1])
 
 def drawObjects(win):
     mainBoard.draw()

0c1ae1a23830e8da22ff90fb0b1faf382201c7ac
Emilio Mendoza <Ironislife39@gmail.com>
2023-01-16 09:18:54 -0500

    Added Scoreboard

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pygame
 from pygame.math import Vector2
+import pygame.font
 import sys
-import math
-import threading
-import time
+
 
 pygame.init()
+pygame.font.init()
 
 WIDTH, HEIGHT = 900, 900
-BACKGROUND_COLOR = (34, 34, 34)#(44,42,42)
+BACKGROUND_COLOR = (34, 34, 34) #(44,42,42)
 PIECE_COLORS: tuple[tuple[int, int, int], tuple[int, int, int]] = ((209,178,129), (20,22,22))
 FRAMERATE = 60
 
@@ -16,9 +16,138 @@ CLOCK = pygame.time.Clock()
 
 Pieces = []
 
+# Font initialization
+scoreboardFont = pygame.font.Font("data/fonts/Montserrat-ExtraBold.ttf", 35)
+
 
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Checkers")
+
+
+# Thanks to PoDuck for the object class
+# Code can be found here: https://github.com/PoDuck/pygame_outlined_text
+class OutlinedText(object):
+    def __init__(
+            self,
+            text,
+            position,
+            outline_width,
+            font_size,
+            screen,
+            foreground_color=(255, 255, 255),
+            background_color=(0, 0, 0)
+    ):
+        """
+        Outline text for pygame.
+        :param text: bytes or unicode text
+        :param position: tuple of form (x, y) you wish text to be rendered at
+        :param outline_width: outline width in pixels
+        :param font_size: font size
+        :param screen: pygame screen you want text rendered to
+        :param foreground_color: foreground color of text defaults to white
+        :param background_color: background color of text defaults to black
+        """
+        self.text = text
+        self.position = position
+        self.foreground = foreground_color
+        self.background = background_color
+        self.outline_width = outline_width
+        self.screen = screen
+        self.font = pygame.font.Font("data/fonts/Montserrat-ExtraBold.ttf", 40)
+        self.text_surface = self.font.render(self.text, True, self.foreground)
+        self.text_outline_surface = self.font.render(self.text, True, self.background)
+        # There is no good way to get an outline with pygame, so we draw
+        # the text at 8 points around the main text to simulate an outline.
+        self.directions = [
+            (self.outline_width, self.outline_width),
+            (0, self.outline_width),
+            (-self.outline_width, self.outline_width),
+            (self.outline_width, 0),
+            (-self.outline_width, 0),
+            (self.outline_width, -self.outline_width),
+            (0, -self.outline_width),
+            (-self.outline_width, -self.outline_width)
+        ]
+
+    def get_width(self):
+        """
+        Get width of text including border.
+        :return: width of text, including border.
+        """
+        return self.text_surface.get_width() + self.outline_width * 2
+
+    def change_position(self, position):
+        """
+        change position text is blitted to.
+        :param position: tuple in the form of (x, y)
+        :return:
+        """
+        self.position = position
+
+    def change_text(self, text):
+        """
+        Changes text to "text"
+        :param text: New text
+        """
+        self.text = text
+        self._update_text()
+
+    def change_foreground_color(self, color):
+        """
+        Changes foreground color
+        :param color: New foreground color
+        """
+        self.foreground = color
+        self._update_text()
+
+    def change_outline_color(self, color):
+        """
+        Changes the outline color
+        :param color: New outline color
+        """
+        self.background = color
+        self._update_text()
+
+    def _update_text(self):
+        """
+        "protected" function to replace the text surface with a new one based on updated values.
+        """
+        self.text_surface = self.font.render(self.text, True, self.foreground)
+        self.text_outline_surface = self.font.render(self.text, True, self.background)
+
+    def draw(self):
+        # blit outline images to screen
+        for direction in self.directions:
+            self.screen.blit(
+                self.text_outline_surface,
+                (
+                    self.position[0] - direction[0],
+                    self.position[1] - direction[1]
+                )
+            )
+        # blit foreground image to the screen
+        self.screen.blit(self.text_surface, self.position)
+
+
+class ScoreBoard:
+    def __init__(self, font: pygame.font.Font):
+        self.font = font
+        self.Player1 =OutlinedText("Player 1", (400, 60), 3, 35, win, foreground_color=(255, 255, 255), background_color=(255, 0, 0))
+        self.Player1Pos = pygame.math.Vector2(130, 60)
+        self.Player2 = self.font.render("Player 2", False, PIECE_COLORS[0])
+        self.Player2Pos = pygame.math.Vector2(620, 760)
+
+        self.pos = (pygame.math.Vector2(), pygame.math.Vector2())
+        #self.selectedMessage = self.font.render("Your Turn")
+        self.selected = 0
+
+    def toggleSelected(self):
+        self.selected = abs(self.selected - 1)
+
+    def draw(self):
+        #win.blit(self.Player1, self.Player1Pos)
+        #win.blit(self.Player2, self.Player2Pos)
+        self.Player1.draw()
 
 
 class Board:
@@ -131,11 +260,11 @@ def drawObjects(win):
     mainBoard.draw()
     for piece in Pieces:
         piece.draw()
-
+    scoreboard.draw()
     pygame.display.update()
 
 
-
+scoreboard = ScoreBoard(font=scoreboardFont)
 mainBoard = Board()
 
 GameController.GeneratePieces(mainBoard)
